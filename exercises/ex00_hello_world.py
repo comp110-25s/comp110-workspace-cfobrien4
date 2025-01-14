949a49b027ec4e9a866647a1b62d5f9d7ca73205
Cameron O'Brien <cfobrien@unc.edu>
2025-01-14 16:44:21 -0500

    "Fist Ex 2"

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,11 +1,11 @@
 """My first exercise in Comp110"""
 
-_author_ = "730670964"
+__author__ = "730670964"
 
 
 def greet(name: str) -> str:
     """A welcoming first function definition."""
     return "Hello, " + name + "!"
 
-    if _name_ == "_main_":
+    if __name__ == "__main__":
         print(greet(name=input("What is your name? ")))
