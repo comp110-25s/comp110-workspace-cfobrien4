d4b23fd201d7bf83c2d0a9644d56f5b5895b0a6b
Cameron O'Brien <cfobrien@unc.edu>
2025-01-14 13:53:08 -0500

    Finished Exerxise 00

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,11 @@
+"""My first exercise in Comp110"""
+
+_author_ = "730670964"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+    if _name_ == "_main_":
+        print(greet(name=input("What is your name? ")))
